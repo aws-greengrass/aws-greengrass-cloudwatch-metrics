7d31027ad27dac7ddd05229ae0f923377385e9ef
Vaibhav Bhasin <vabhasin@amazon.com>
2022-04-15 15:19:50 -0700

    fix: check if proxy scheme is https

--- create-cert-bundle.py
+++ create-cert-bundle.py
@@ -7,8 +7,18 @@ handler = logging.StreamHandler(sys.stdout)
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
 
-if os.environ.get("HTTPS_PROXY") is None:
-    exit()
+proxy_url = os.environ.get("ALL_PROXY")
+if proxy_url is None:
+    # No proxy configured
+    sys.exit(0)
+
+# Just try to check if proxy scheme is http or https
+# This does not validate the full url format
+scheme = proxy_url.split(":")[0]
+if scheme != "https":
+    # Not https proxy
+    sys.exit(0)
+
 logger.info("Creating certificate bundle with proxy root CA, and installing dependencies")
 try:
     import certifi
@@ -18,21 +28,18 @@ except ImportError:
         from pip._vendor import certifi
     except Exception as e:
         logger.exception("Error creating certificate bundle with proxy root CA")
-        exit()
+        sys.exit(1)
 
 p1 = certifi.where()
 p2 = os.environ.get("GG_ROOT_CA_PATH")
 p3 = './ca-bundle.crt'
 try:
-    with open(p1, 'r') as f1, open(p2, 'r') as f2, open(p3, 'a+') as f3:
+    with open(p1, 'r') as f1, open(p2, 'r') as f2, open(p3, 'w') as f3:
         f3.write(f1.read())
         f3.write(f2.read())
-        f1.seek(0)
-        f2.seek(0)
-        f3.seek(0)
 except Exception:
     logger.exception("Error creating certificate bundle with proxy root CA")
-    exit()
+    sys.exit(1)
 try:
     from pip import main as pip
 
@@ -40,3 +47,4 @@ try:
 except Exception:
     logger.exception(
         "Error installing dependencies. Please set 'UseInstaller' to 'False' and pre-install component dependencies")
+    sys.exit(1)
